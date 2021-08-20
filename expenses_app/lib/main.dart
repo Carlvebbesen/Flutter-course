0121697cca185beb7713544cd74531b06efbf4a6
Carl Valdemar Ebbesen <ecvebbesen@gmail.com>
2021-08-20 12:24:17 +0200

    adaptive app for android and ios

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -1,10 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_complete_guide/widgets/chart.dart';
 import 'package:flutter_complete_guide/widgets/transaction_list.dart';
 import './models/transaction.dart';
 import './widgets/new_transaction.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -74,37 +83,74 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Personal Expenses',
-          ),
-          actions: [
-            IconButton(
-                onPressed: () => startAddNewTransaction(context),
-                icon: Icon(Icons.add))
-          ],
-        ),
-        body: SingleChildScrollView(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            Chart(
-              _userTransactions
-                  .where((element) => element.date.isAfter(
-                        DateTime.now().subtract(Duration(days: 7)),
-                      ))
-                  .toList(),
+    final PreferredSizeWidget appbar = Platform.isIOS
+        ? CupertinoNavigationBar(
+            middle: Text(
+              'Personal Expenses',
             ),
-            TransactionList(_userTransactions, removeTransaction),
-          ]),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton: Container(
-          margin: EdgeInsets.fromLTRB(0, 0, 15, 15),
-          child: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () => startAddNewTransaction(context),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  child: Icon(CupertinoIcons.add),
+                  onTap: () => startAddNewTransaction(context),
+                )
+              ],
+            ),
+          )
+        : AppBar(
+            title: Text(
+              'Personal Expenses',
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () => startAddNewTransaction(context),
+                  icon: Icon(Icons.add))
+            ],
+          );
+
+    final pageBody = SafeArea(
+        child: SingleChildScrollView(
+      child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        Container(
+          height: (MediaQuery.of(context).size.height -
+                  appbar.preferredSize.height -
+                  MediaQuery.of(context).padding.top) *
+              0.3,
+          child: Chart(
+            _userTransactions
+                .where((element) => element.date.isAfter(
+                      DateTime.now().subtract(Duration(days: 7)),
+                    ))
+                .toList(),
           ),
-        ));
+        ),
+        Container(
+            height: (MediaQuery.of(context).size.height -
+                    appbar.preferredSize.height -
+                    MediaQuery.of(context).padding.top) *
+                0.7,
+            child: TransactionList(_userTransactions, removeTransaction)),
+      ]),
+    ));
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            child: pageBody,
+            navigationBar: appbar,
+          )
+        : Scaffold(
+            appBar: appbar,
+            body: pageBody,
+            floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+            floatingActionButton: Platform.isIOS
+                ? Container()
+                : Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 15, 15),
+                    child: FloatingActionButton(
+                      child: Icon(Icons.add),
+                      onPressed: () => startAddNewTransaction(context),
+                    ),
+                  ));
   }
 }
--- expenses_app/lib/widgets/transaction_list.dart
+++ expenses_app/lib/widgets/transaction_list.dart
@@ -10,56 +10,52 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 450,
-      child: _userTransactions.isEmpty
-          ? Column(
-              children: [
-                Text(
-                  "Not transactions added yet!",
-                  style: Theme.of(context).textTheme.title,
+    return _userTransactions.isEmpty
+        ? Column(
+            children: [
+              Text(
+                "Not transactions added yet!",
+                style: Theme.of(context).textTheme.title,
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(height: 20),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                      leading: CircleAvatar(
-                          radius: 30,
-                          child: Padding(
-                            padding: const EdgeInsets.all(6.0),
-                            child: FittedBox(
-                              child:
-                                  Text("\$${_userTransactions[index].amount}"),
-                            ),
-                          )),
-                      title: Text(
-                        _userTransactions[index].amount.toString(),
-                        style: Theme.of(context).textTheme.title,
-                      ),
-                      subtitle: Text(DateFormat.yMMMd()
-                          .format(_userTransactions[index].date)),
-                      trailing: IconButton(
-                        color: Theme.of(context).errorColor,
-                        icon: Icon(Icons.delete),
-                        onPressed: () =>
-                            deleteTransaction(_userTransactions[index].id),
-                      )),
-                );
-              },
-              itemCount: _userTransactions.length,
-            ),
-    );
+              )
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                    leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: FittedBox(
+                            child: Text("\$${_userTransactions[index].amount}"),
+                          ),
+                        )),
+                    title: Text(
+                      _userTransactions[index].amount.toString(),
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(DateFormat.yMMMd()
+                        .format(_userTransactions[index].date)),
+                    trailing: IconButton(
+                      color: Theme.of(context).errorColor,
+                      icon: Icon(Icons.delete),
+                      onPressed: () =>
+                          deleteTransaction(_userTransactions[index].id),
+                    )),
+              );
+            },
+            itemCount: _userTransactions.length,
+          );
   }
 }
